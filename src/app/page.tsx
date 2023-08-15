dccacdc1cac7d8e82a38edd5c6d47fad11dc349d
Estifanos <estifanosbeyene21@gmail.com>
2023-08-15 14:55:00 -0400

    feat: add login page
    redirect user to login page if no session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,18 @@ import { AuthButtonClient } from "@/components/molecules/auth-button-client";
 import { AuthButtonServer } from "@/components/molecules/auth-button-server";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect("/login");
+  }
+
   const { data: tweets } = await supabase.from("tweets").select();
 
   return (
