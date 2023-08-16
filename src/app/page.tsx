4da7ce10ae4c70f260acfeff45674bb05de2528b
Estifanos <estifanosbeyene21@gmail.com>
2023-08-16 13:32:34 -0400

    feat: show like/unlike button home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { AuthButtonServer } from "@/components/molecules/auth-button-server";
+import { Likes } from "@/components/molecules/likes";
 import { NewTweet } from "@/components/molecules/new-tweet";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
@@ -14,14 +15,30 @@ export default async function Home() {
     redirect("/login");
   }
 
-  const { data: tweets } = await supabase
+  const { data } = await supabase
     .from("tweets")
-    .select("*, profiles(*)");
-
+    .select("*, profiles(*), likes(*)");
+  const tweets =
+    data?.map((tweet) => ({
+      ...tweet,
+      user_has_liked_tweet: !!tweet.likes.find(
+        (like) => like.user_id === session.user.id,
+      ),
+      likes: tweet.likes.length,
+    })) ?? [];
   return (
     <div>
       <AuthButtonServer />
       <NewTweet />
+      {tweets?.map((tweet) => (
+        <div key={tweet.id}>
+          <p>
+            {tweet?.profiles?.name} {tweet?.profiles?.username}
+          </p>
+          <p>{tweet?.title}</p>
+          <Likes tweet={tweet} />
+        </div>
+      ))}
       <pre>{JSON.stringify(tweets, null, 2)}</pre>
     </div>
   );
